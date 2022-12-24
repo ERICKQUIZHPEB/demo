7d75b15e37c0a29040f2a63d7d59649451e28524
Erick <96554516+ERICKQUIZHPEB@users.noreply.github.com>
2022-12-23 19:33:13 -0500

    Add files via upload

---
+++ src/main/kotlin/com/example/evaluacion/controller/AssistantController.kt
@@ -0,0 +1,44 @@
+package com.example.evaluacion.controller
+
+import com.example.evaluacion.model.Assistant
+//import com.example.proyectdg.model.Assistant
+import com.example.evaluacion.service.AssistantService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+
+@RestController
+@RequestMapping("/assistant")
+
+class AssistantController {
+    @Autowired
+    lateinit var assistantService: AssistantService
+
+    @PostMapping
+    fun save (@RequestBody @Valid assistant: Assistant):Assistant{
+        return assistantService.save(assistant)
+    }
+
+    @GetMapping
+    fun list ():List<Assistant>{
+        return assistantService.list()
+    }
+
+    @PutMapping
+    fun update (@RequestBody assistant:Assistant):ResponseEntity<Assistant>{
+        return ResponseEntity(assistantService.update(assistant), HttpStatus.OK)
+    }
+
+    @PatchMapping
+    fun updateName (@RequestBody assistant:Assistant): ResponseEntity<Assistant>{
+        return ResponseEntity(assistantService.updateName(assistant), HttpStatus.OK)
+    }
+
+    @DeleteMapping("/delete/{id}")
+    fun delete (@PathVariable("id") id: Long):Boolean?{
+        return assistantService.delete(id)
+    }
+}
